b6008226fa439239c4226e01911813973295eece
Carlos Maximilliano <79182015+carlosmax21@users.noreply.github.com>
2023-05-28 14:16:55 +0700

    update website

--- Stock_Overview.py
+++ Stock_Overview.py
@@ -6,6 +6,7 @@ import datetime as dt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 from ta.momentum import RSIIndicator
+from ta.trend import MACD
 
 # Set Streamlit layout
 st.set_page_config(layout="wide")
@@ -21,24 +22,8 @@ def get_stock_info(symbol):
     info = stock.info
     return info
 
-# Calculate ROI
-def calculate_roi(stock_data):
-    initial_price = stock_data['Adj Close'].iloc[0]
-    final_price = stock_data['Adj Close'].iloc[-1]
-    roi = (final_price - initial_price) / initial_price
-    return roi
-
-# Calculate CAGR
-def calculate_cagr(stock_data):
-    trading_days = pd.date_range(start=stock_data.index.min(), end=stock_data.index.max(), freq='B')  # 'B' for business days
-    n = len(trading_days)  # Number of trading days in the given date range
-    initial_price = stock_data['Adj Close'].iloc[0]
-    final_price = stock_data['Adj Close'].iloc[-1]
-    cagr = (final_price / initial_price) ** (1 / (n / 252)) - 1
-    return cagr
-
 # Set title and sidebar
-st.header("Stock Overview")
+st.title("Stock Overview")
 st.sidebar.markdown("Stock Range")
 
 # Get user input
@@ -62,62 +47,33 @@ stock_info = get_stock_info(selected_symbol)
 long_name = stock_info['longName'] if stock_info else selected_symbol
 
 # Display stock information
-st.write("Symbol: ", stock_info["symbol"])
-st.write("Company: ", long_name)
-st.write("Sector: ", stock_info["sector"])
-st.write("Industry: ", stock_info["industry"])
-st.write("Country: ", stock_info["country"])
+st.header("Stock Detail")
+st.write(long_name, "(", stock_info["symbol"], ")", " - ", stock_info["country"])
+st.write(stock_info["sector"], " - ", stock_info["industry"])
 st.write(stock_info["longBusinessSummary"])
 
 if stock_data is not None:
-    st.write("Stock Data:")
+    st.header("Stock Data:")
+    st.write("Showing the latest data from stock")
     st.dataframe(stock_data.tail())
+    
+    st.subheader("Technical Indicator")
+    st.write("Technical indicators help analyze stock price trends and identify potential opportunities. Here's a brief explanation of some indicators used:")
 
-    # Calculate and display performance metrics
-    stock_data['Daily Returns'] = stock_data['Adj Close'].pct_change()
-    stock_data['Cumulative Returns'] = (1 + stock_data['Daily Returns']).cumprod() - 1
-    volatility = stock_data['Daily Returns'].std()
-    roi = calculate_roi(stock_data)
-    cagr = calculate_cagr(stock_data)
+    # Explanation of RSI
+    st.write("**RSI (Relative Strength Index):**")
+    st.write("RSI is a momentum oscillator that measures the speed and change of price movements. It oscillates between 0 and 100, with values above 70 indicating overbought conditions and values below 30 indicating oversold conditions.")
 
-    st.subheader("Performance Metrics")
-    st.write(f"ROI: {roi:.4f}")
-    st.write(f"Volatility (Standard Deviation): {volatility:.4f}")
-    st.write(f"CAGR: {cagr:.4f}")
+    # Explanation of SMA
+    st.write("**SMA (Simple Moving Average):**")
+    st.write("SMA calculates the average price of a security over a specified time period. It helps identify trends by smoothing out price fluctuations and can be used to determine support and resistance levels.")
 
-    # Calculate and display RSI
-    rsi_period = 14  # RSI calculation period
-    rsi = RSIIndicator(stock_data['Adj Close'], rsi_period)
-    stock_data['RSI'] = rsi.rsi()
+    # Explanation of MACD
+    st.write("**MACD (Moving Average Convergence Divergence):**")
+    st.write("MACD is a trend-following momentum indicator that consists of two lines: the MACD line and the signal line. MACD crossovers and divergences can indicate potential buy or sell signals.")
 
-    # Create subplots
-    fig_peform = make_subplots(rows=1, cols=2, subplot_titles=("Cumulative Returns", "RSI"))
-
-    # Add cumulative returns plot
-    fig_peform.add_trace(
-        go.Scatter(x=stock_data.index, y=stock_data['Cumulative Returns'], name="Cumulative Returns"),
-        row=1, col=1
-    )
-
-    # Add RSI plot
-    fig_peform.add_trace(
-        go.Scatter(x=stock_data.index, y=stock_data['RSI'], name="RSI"),
-        row=1, col=2
-    )
-
-    # Update layout
-    fig_peform.update_layout(
-        title=f"{long_name} Performance",
-        xaxis_rangeslider_visible=False,
-        showlegend=True,
-        height=600,
-        template='plotly_dark'
-    )
-
-    st.plotly_chart(fig_peform, use_container_width=True)
 
     fig_general = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0.05)
-
     # Add candlestick chart
     fig_general.add_trace(
         go.Candlestick(x=stock_data.index,
@@ -128,24 +84,20 @@ if stock_data is not None:
                        name="Candlestick"),
         row=1, col=1
     )
-
     # Update candlestick chart properties
     fig_general.update_traces(decreasing_line_color='red', increasing_line_color='green', row=1, col=1)
-
     # Add 20-day SMA
     sma_20 = stock_data['Adj Close'].rolling(window=20).mean()
     fig_general.add_trace(
         go.Scatter(x=stock_data.index, y=sma_20, name="20-day SMA", line=dict(color='blue')),
         row=1, col=1
     )
-
     # Add 100-day SMA
     sma_100 = stock_data['Adj Close'].rolling(window=100).mean()
     fig_general.add_trace(
         go.Scatter(x=stock_data.index, y=sma_100, name="100-day SMA", line=dict(color='yellow')),
         row=1, col=1
     )
-
     # Add line chart for adjusted close price
     fig_general.add_trace(
         go.Scatter(x=stock_data.index, y=stock_data['Adj Close'], name="Adjusted Close"),
@@ -163,16 +115,54 @@ if stock_data is not None:
     # Update volume chart properties
     fig_general.update_yaxes(title_text="Volume", row=2, col=1)
 
-    # Update layout
-    fig_general.update_layout(
-        title=f"{long_name} Stock Data",
-        xaxis_rangeslider_visible=False,
-        showlegend=True,
-        height=900,
-        template='plotly_dark'
+    # Create a separate figure for RSI
+    fig_rsi = make_subplots(rows=1, cols=1, shared_xaxes=True)
+
+    # Calculate RSI
+    rsi_period = 14  # Number of periods for RSI calculation
+    rsi = RSIIndicator(close=stock_data['Adj Close'], window=rsi_period)
+    rsi_values = rsi.rsi()
+
+    # Add RSI line chart
+    fig_rsi.add_trace(
+        go.Scatter(x=stock_data.index, y=rsi_values, name="RSI", line=dict(color='orange')),
+        row=1, col=1
     )
 
-    # Display the chart
+    # Update RSI chart properties
+    fig_rsi.update_yaxes(title_text="RSI", row=1, col=1)
+    
+    # Create a separate figure for MACD
+    fig_macd = make_subplots(rows=1, cols=1, shared_xaxes=True)
+
+    # Calculate MACD
+    macd = MACD(close=stock_data['Adj Close'])
+    macd_values = macd.macd()
+    signal_line = macd.macd_signal()
+    histogram = macd.macd_diff()
+
+    # Add MACD line chart
+    fig_macd.add_trace(
+        go.Scatter(x=stock_data.index, y=macd_values, name="MACD", line=dict(color='purple')),
+        row=1, col=1
+    )
+    # Add signal line
+    fig_macd.add_trace(
+        go.Scatter(x=stock_data.index, y=signal_line, name="Signal Line", line=dict(color='yellow')),
+        row=1, col=1
+    )
+    # Add histogram
+    fig_macd.add_trace(
+        go.Bar(x=stock_data.index, y=histogram, name="Histogram", marker_color='rgba(0, 255, 0, 0.5)'),
+        row=1, col=1
+    )
+
+    # Update MACD chart properties
+    fig_macd.update_yaxes(title_text="MACD", row=1, col=1)
+
+    # Display the charts
     st.plotly_chart(fig_general, use_container_width=True)
+    st.plotly_chart(fig_rsi, use_container_width=True)
+    st.plotly_chart(fig_macd, use_container_width=True)
 else:
     st.write("No data available for the selected stock symbol and date range.")
--- pages/Predict_Stock.py
+++ pages/Predict_Stock.py
@@ -7,11 +7,12 @@ import tensorflow as tf
 import pickle
 import plotly.graph_objects as go
 from sklearn.preprocessing import MinMaxScaler
+import scipy.stats as stats
 
+# Title
 st.title("Stock Prediction")
 
-
-# Fetch file
+# Load models and scalers
 with open('scaler/tlkm_scaler.pkl', 'rb') as file:
     tlkm_scaler = pickle.load(file)
 with open('scaler/xl_scaler.pkl', 'rb') as file:
@@ -26,9 +27,11 @@ xl_model = tf.keras.models.load_model('model/EXCL_best_model.h5')
 tbig_model = tf.keras.models.load_model('model/TBIG_best_model.h5')
 towr_model = tf.keras.models.load_model('model/TOWR_best_model.h5')
 
+# Stock symbols
 stock_symbols = ["TLKM.JK", "EXCL.JK", "TBIG.JK", "TOWR.JK"]
 selected_symbol = st.sidebar.selectbox('Select a stock symbol:', stock_symbols)
 
+# Select model and scaler based on the symbol
 def select_model_and_scaler(symbol):
     if symbol == "TLKM.JK":
         return tlkm_model, tlkm_scaler
@@ -39,48 +42,47 @@ def select_model_and_scaler(symbol):
     elif symbol == "TOWR.JK":
         return towr_model, towr_scaler
     
+model, scaler = select_model_and_scaler(selected_symbol)
+
+# Fetch stock data
 def fetch_stock_data(symbol, start_date, end_date):
     stock = yf.download(symbol, start=start_date, end=end_date)
     return stock
-
-def scale_data(data, scaler):
-    data = data.filter(['Adj Close'])
-    adj_close_data = data.values
-    
-    scaled_data = scaler.fit_transform(adj_close_data)
-    
-    return scaled_data
-
-def predict_future(model, scaler, scaled_data, future_days):    
-    predictions = []
-    
-    for _ in range(future_days):
-        # Take the last 60 days' data for prediction
-        input_data = scaled_data[-60:, :]
-        
-        # Reshape the input data for the LSTM model
-        input_data = input_data.reshape((1, input_data.shape[0], input_data.shape[1]))
-        
-        # Make the prediction
-        prediction = model.predict(input_data)
-        inverse_prediction = scaler.inverse_transform(prediction)
-        
-        # Append the prediction to the list
-        predictions.append(inverse_prediction[0][0])
-        
-        # Update the data array with the predicted value
-        scaled_data = scaled_data[1:]
-        scaled_data = np.append(scaled_data, [[prediction][0][0]], axis=0)
-        
-    return predictions
-    
-model, scaler = select_model_and_scaler(selected_symbol)
 
 end_date = dt.date.today()
 start_date = end_date - dt.timedelta(days=365)
 
 stock_data = fetch_stock_data(selected_symbol, start_date, end_date)
 
+# Scale data
+def scale_data(data, scaler):
+    data = data.filter(['Adj Close'])
+    adj_close_data = data.values
+    scaled_data = scaler.fit_transform(adj_close_data)
+    return scaled_data
+
+scaled_data = scale_data(stock_data, scaler)
+
+# Predict future stock prices
+def predict_future(model, scaler, scaled_data, future_days):
+    predictions = []
+    for _ in range(future_days):
+        input_data = scaled_data[-60:, :]
+        input_data = input_data.reshape((1, input_data.shape[0], input_data.shape[1]))
+        prediction = model.predict(input_data)
+        inverse_prediction = scaler.inverse_transform(prediction)
+        predictions.append(inverse_prediction[0][0])
+        scaled_data = scaled_data[1:]
+        scaled_data = np.append(scaled_data, [[prediction][0][0]], axis=0)
+    return predictions
+
+predict_stock_price = predict_future(model, scaler, scaled_data, 30)
+
+# Create dataframe for predicted stock prices
+predict_stock_price_df = pd.DataFrame(predict_stock_price, columns=['Prediction'])
+predict_stock_price_df.index = range(1, len(predict_stock_price_df) + 1)
+
+# Plot stock data
 fig = go.Figure(data=go.Scatter(x=stock_data.index, y=stock_data['Adj Close']))
 fig.update_layout(
     title=f"Stock Adjusted Close",
@@ -92,15 +94,7 @@ fig.update_layout(
 )
 st.plotly_chart(fig, use_container_width=True)
 
-scaled_data = scale_data(stock_data, scaler)
-
-predict_stock_price = predict_future(model, scaler, scaled_data, 90)
-
-predict_stock_price_df = pd.DataFrame(predict_stock_price, columns=['Prediction'])
-predict_stock_price_df.index = range(1, len(predict_stock_price_df) + 1)
-
-st.write(predict_stock_price_df)
-
+# Plot predicted stock prices
 fig_pred = go.Figure(data=go.Scatter(x=predict_stock_price_df.index, y=predict_stock_price_df['Prediction']))
 fig_pred.update_layout(
     title=f"Stock Prediction",
@@ -111,3 +105,11 @@ fig_pred.update_layout(
     template='plotly_dark'
 )
 st.plotly_chart(fig_pred, use_container_width=True)
+
+mean_returns = np.mean(predict_stock_price)
+std_returns = np.std(predict_stock_price)
+confidence_level = 0.95
+var = abs(stats.norm.ppf(1 - confidence_level, mean_returns, std_returns))
+
+# Display VaR
+st.write(f"Value at Risk (VaR) at {confidence_level*100}% confidence level: {var}")
